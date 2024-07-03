5a62e832b68380063f7e28c62690b92c319124b1
sajinpaula92 <sajinpaulab92@gmail.com>
2024-07-03 19:39:00 +0200

    Basics of JS

--- firstlesson.js
+++ firstlesson.js
@@ -1,1 +1,26 @@
 // javascript commetns always starts with just two slashes.
+
+// The first lesson is datea types
+ //1. The first data type is string. it should be always typed in double quotes. ForExample 
+
+ "SAJIN"
+
+ "SAJIN"
+
+ typeof // typeof is a keyword which shows what type of character is that for example, whether it is string or number or boolean. Boolean means true or false.
+
+ // Variables are defined with let. If the variable contains more than one word it should be written in camel cases. Which means first word starts with small letter and the second and more words starts with Capital letter.
+
+ // let myVariable= This is how we list variables.
+
+ // The second data type is number.
+
+ // The third data type is boolean.
+
+ // The fourth data type is undefined.
+
+ // If we need to define equal to it should be given as 3 equal to signs. ===.
+
+
+
+
