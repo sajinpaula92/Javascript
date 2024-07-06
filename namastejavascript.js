7e266d828d9995c3f4e81302179b326e4fb77fdb
sajinpaula92 <sajinpaulab92@gmail.com>
2024-07-06 23:25:45 +0200

    Namaste Javascript 1

---
+++ namastejavascript.js
@@ -0,0 +1,31 @@
+// Learned more about closures.
+
+//Functions 
+
+    // Function Statement
+
+    //Function Expression
+
+    //Function Declaration
+
+    //Anonymous Function ---- A function without a name is called anonymous function.
+
+    //Named Function Expression
+
+    //Difference between Parameters and Arguments
+
+    //First Class Function - Ability to be used like values or arguments. It is also called First class Citizens.
+
+    //Arrow Function
+
+
+
+
+    ////Event loop
+
+    //Event Listeners
+
+    
+
+
+
