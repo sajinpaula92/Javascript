72be213ad5a6fe6d68c4e19219c79e008c711de1
sajinpaula92 <sajinpaulab92@gmail.com>
2024-07-24 23:39:17 +0200

    Day 4 loops

--- supersimple dev/Scripts/11-todo-list.js
+++ supersimple dev/Scripts/11-todo-list.js
@@ -1,3 +1,40 @@
+/*const todoList = [];
+
+function addTodo() {
+
+    const  inputElement =  document.querySelector('.js-name-input');
+    const name = inputElement.value;
+
+    todoList.push(name);
+    console.log(todoList);
+
+    inputElement.value = '';
+
+}*/
+
+//second todo list:
+
+const todoList1 = ['make dinner', 'wash dishes'];
+
+renderTodoList();
+
+function renderTodoList() {
+
+
+let todoListHTML = '';
+
+for(let i =0; i< todoList1.length; i++) {
+   const todo = todoList1[i];
+   const html = `<p> ${todo} <p/>`;
+   todoListHTML += html;
+}
+
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+}
+
 const todoList = [];
 
 function addTodo() {
@@ -10,8 +47,5 @@ function addTodo() {
 
     inputElement.value = '';
 
+    renderTodoList();
 }
-
-//second todo list:
-
-const todoList = ['make dinner', 'wash dishes'];
